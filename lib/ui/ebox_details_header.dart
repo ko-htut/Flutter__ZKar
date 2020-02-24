5329a275c90d8570b708070a1be45d7d4663d4d9
Min Than Htut <minthanhtut@Mins-MBP.lan>
2020-02-24 14:52:10 +0630

    detail changes

--- lib/ui/ebox_details_header.dart
+++ lib/ui/ebox_details_header.dart
@@ -11,7 +11,7 @@ class EboxDetailsHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 120),
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 120),
       child: Container(
         // color: Colors.white30,
         child: Stack(children: <Widget>[
